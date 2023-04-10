d5ed81d6d4bfca3832ac759995c13c6c933616d1
ashirovjonibek <ashirov.uzbek.1997@gmail.com>
2023-04-10 10:36:30 +0500

    graticule

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import {Feature, Map, Overlay, View} from 'ol';
+import {Feature, Graticule, Map, Overlay, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {TileWMS} from "ol/source";
@@ -17,7 +17,7 @@ import {
 import Chart from 'chart.js/auto';
 
 
-let value="shurtan"
+let value = "shurtan"
 
 function showInfo(event) {
 
@@ -40,7 +40,7 @@ function showInfo(event) {
     const canvas = document.createElement('canvas');
     canvas.width = 100;
     canvas.height = 100;
-    canvas.textContent=feature.values_.Name
+    canvas.textContent = feature.values_.Name
     const ctx = canvas.getContext('2d');
     let r = random();
     const data = {
@@ -68,11 +68,11 @@ function showInfo(event) {
     map.addOverlay(overlay);
 
     // Remove the overlay when the user clicks outside of it
-    map.once('click', function() {
+    map.once('click', function () {
         map.removeOverlay(overlay);
     });
     const pixel = map.getPixelFromCoordinate(event.coordinate);
-    map.forEachFeatureAtPixel(pixel, function(feature) {
+    map.forEachFeatureAtPixel(pixel, function (feature) {
         const properties = feature.getProperties();
         const name = properties['Name'];
         const info = document.createElement('div');
@@ -82,89 +82,98 @@ function showInfo(event) {
 }
 
 function random() {
-    let data={a:0,b:0};
-    data.a=Math.floor(Math.random() * 101);
+    let data = {a: 0, b: 0};
+    data.a = Math.floor(Math.random() * 101);
     console.log(data)
-    data.b=100-data.a;
+    data.b = 100 - data.a;
     console.log(data)
     return data;
 }
 
 
+const iconStyle = new Style({
+    image: new Icon({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: 'armatura.svg',
+        // size: [111, 170]
+    }),
+});
 
-    const iconStyle = new Style({
-        image: new Icon({
-            anchor: [0.5, 46],
-            anchorXUnits: 'fraction',
-            anchorYUnits: 'pixels',
-            src: 'armatura.svg',
-            // size: [111, 170]
-        }),
-    });
 
+const wmsSource = new TileWMS({
+    url: 'http://localhost:8080/geoserver/cite/wms',
+    params: {'LAYERS': 'cite:--point', 'TILED': true},
+    serverType: 'geoserver',
+    transition: 0,
+})
 
-    const wmsSource = new TileWMS({
-        url: 'http://localhost:8080/geoserver/cite/wms',
-        params: {'LAYERS': 'cite:--point', 'TILED': true},
-        serverType: 'geoserver',
-        transition: 0,
-    })
+const vectorPolygons = new VectorLayer({
+    source: new VectorSource({
+        url: './coordinates/' + value + '.geojson',
+        format: new GeoJSON(),
+    }),
+    style: function (feature) {
+        let circleStyle = iconStyle;
+        // Create a Text style for the label
+        let textStyle = new Style({
+            text: new Text({
+                text: feature.get('Name').replace(/\D/g, ""),
+                fill: new Fill({
+                    color: 'black'
+                }),
+                stroke: new Stroke({
+                    color: 'white',
+                    width: 3
+                }),
+                offsetY: -10 // Offset the label above the feature
+            })
+        });
+        // Return an array of styles with the circle and text styles
+        return [circleStyle, textStyle];
+    }
+});
 
-    const vectorPolygons = new VectorLayer({
-        source: new VectorSource({
-            url: './coordinates/'+value+'.geojson',
-            format: new GeoJSON(),
-        }),
-        style: function (feature) {
-            let circleStyle = iconStyle;
-            // Create a Text style for the label
-            let textStyle = new Style({
-                text: new Text({
-                    text: feature.get('Name').replace(/\D/g, ""),
-                    fill: new Fill({
-                        color: 'black'
-                    }),
-                    stroke: new Stroke({
-                        color: 'white',
-                        width: 3
-                    }),
-                    offsetY: -10 // Offset the label above the feature
-                })
-            });
-            // Return an array of styles with the circle and text styles
-            return [circleStyle, textStyle];
-        }
-    });
+const view = new View({
+    center: [66.00936586257896, 38.51123601575114],
+    zoom: 12,
+    projection: "EPSG:4326"
+});
 
-    const view = new View({
-        center: [66.00936586257896, 38.51123601575114],
-        zoom: 12,
-        projection: "EPSG:4326"
-    });
+const map = new Map({
+    target: 'map',
+    layers: [
+        // new TileLayer({
+        //   source: new OSM()
+        // }),
+        vectorPolygons,
+    ],
+    view: view
+});
 
-    const map = new Map({
-        target: 'map',
-        layers: [
-            // new TileLayer({
-            //   source: new OSM()
-            // }),
-            vectorPolygons,
-        ],
-        view: view
-    });
+let graticule = new Graticule({
+    strokeStyle: new Stroke({
+        color: "grey",
+        width: 1.5,
+        lineDash: [0.5, 4]
+    }),
+    showLabels: true
+});
+graticule.setMap(map);
 
-    map.on('singleclick', showInfo);
+map.on('singleclick', showInfo);
 
-    map.on('pointermove', function(event) {
-        const feature = map.getFeaturesAtPixel(event.pixel)[0];
-        if (feature) {
-            iconStyle.getImage().setScale(1.5); // Increase scale factor on hover
-            map.getViewport().style.cursor = 'pointer'; // Set cursor to pointer
-        } else {
-            iconStyle.getImage().setScale(1); // Restore original scale factor
-            map.getViewport().style.cursor = ''; // Reset cursor
-        }
-    });
+map.on('pointermove', function (event) {
+    const feature = map.getFeaturesAtPixel(event.pixel)[0];
+    if (feature) {
+        iconStyle.getImage().setScale(1.5); // Increase scale factor on hover
+        map.getViewport().style.cursor = 'pointer'; // Set cursor to pointer
+    } else {
+        iconStyle.getImage().setScale(1); // Restore original scale factor
+        map.getViewport().style.cursor = ''; // Reset cursor
+    }
+});
 
 
 function updateVectorSource(value) {
@@ -177,10 +186,10 @@ function updateVectorSource(value) {
 }
 
 // Assign the updateVectorSource function to the click event of the button
-document.querySelector('#change-source-button').addEventListener('click', function() {
+document.querySelector('#change-source-button').addEventListener('click', function () {
     const value = document.querySelector('#source-select').value;
     updateVectorSource(value);
 });
 
-    Object.assign(window, {map})
+Object.assign(window, {map})
 
